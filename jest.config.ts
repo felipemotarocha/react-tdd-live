6eba2d2a20461a34349c8ba2f63149ffe5ac7b61
Felipe Rocha <felipemota.rocha@gmail.com>
2023-04-22 17:20:21 -0300

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,5 @@
+export default {
+  clearMocks: true,
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>hello world</h1>;
+};
+
+export default App;
