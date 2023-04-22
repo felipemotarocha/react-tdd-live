3906b5da3d6ed2dbea52b6c032b0bfbb5b377b13
Felipe Rocha <felipemota.rocha@gmail.com>
2023-04-22 18:20:36 -0300

    add talwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Todos from "./components/todos.component";
+
 const App = () => {
-  return <h1>hello world</h1>;
+  return <Todos />;
 };
 
 export default App;
---
+++ src/components/todos.component.tsx
@@ -0,0 +1,5 @@
+const Todos = (): JSX.Element => {
+  return <div className="w-screen h-screen bg-gray-950"></div>;
+};
+
+export default Todos;
